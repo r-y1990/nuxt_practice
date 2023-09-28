410a7cbd05b45fcb2f410106eb84a533586d5736
r-y1990 <kiiroitori1990.02@gmail.com>
2023-09-28 13:57:31 +0000

    First Commit

---
+++ layouts/custom.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-app>
+        <div>
+            <header>
+                <Header />
+            </header>
+            <v-main>
+                <slot />
+            </v-main>
+        </div>
+    </v-app>
+</template>
+
+<script setup>
+import Header from "@/components/header/Header.vue";
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+import { DefineNuxtConfig } from "nuxt"
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ['vuetify/lib/styles/main.sass'],
+  build: {
+    transpile: ['vuetify']
+  }
+})
